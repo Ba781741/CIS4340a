69861d137719861e9c91c04d1023b3f0e7796c29
Ba781741 <abujazar.badr@gmail.com>
2025-03-02 16:40:13 -0500

    Update R03 NUM03 J.java

--- R03 NUM03 J.java
+++ R03 NUM03 J.java
@@ -1,9 +1,5 @@
-public static int getInteger(DataInputStream is) throws IOException {
-  return is.readInt(); 
-}
-
-Right-
-
 public static long getInteger(DataInputStream is) throws IOException {
   return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
+
+
